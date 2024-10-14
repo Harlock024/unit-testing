73fbdb17d67633823c618c712454e46e20f960fa
harlock024 <hadith024@gmail.com>
2024-10-13 21:27:49 -0600

    add factorial branch (#4)

--- src/app/factorial/factorial.ts
+++ src/app/factorial/factorial.ts
@@ -1,5 +1,5 @@
-export function factorial(n: number): number {
-  let r = 1;
-  if (n === 0) return 1;
-  return n * factorial(n - 1);
+export function factorial(x: number): number {
+  if (x < 0 || x > 15) return 0;
+  if (x === 0) return 1;
+  return x * factorial(x - 1);
 }
