41b245ff5d8471ba20a95cc5d37708f856c46142
Hadith024 <144747775+Harlock024@users.noreply.github.com>
2024-10-17 01:50:28 -0600

    Stddev (#10)
    * stddev branch added (#9)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { MediaComponent } from "./media/media.component";
+import { StddevComponent } from "./stddev/stddev.component";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, MediaComponent],
+  imports: [RouterOutlet, MediaComponent, StddevComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
-export class AppComponent {}
+export class AppComponent {
+  title = "unit-testing";
+}
