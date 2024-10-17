09e195532dec72dc0bad706ee0e40991d02cbad2
harlock024 <hadith024@gmail.com>
2024-10-17 01:04:54 -0600

    stddev branch added, test  (#9)

---
+++ src/app/stddev/stddev.component.ts
@@ -0,0 +1,15 @@
+import { Component,  } from "@angular/core";
+import cols from
+
+@Component({
+  selector: "app-stddev",
+  standalone: true,
+  imports: [],
+  templateUrl: "./stddev.component.html",
+  styleUrl: "./stddev.component.css",
+})
+export class StddevComponent {
+
+
+
+}
