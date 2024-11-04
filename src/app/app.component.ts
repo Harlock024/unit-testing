35bd286aa17c8664e4204cdf818d0c5c8b43a56d
Hadith024 <144747775+Harlock024@users.noreply.github.com>
2024-11-04 01:10:34 -0600

    UI (#21)
    * menu base integration testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { MediaComponent } from "./media/media.component";
-import { StddevComponent } from "./stddev/stddev.component";
+import { UiComponent } from "./ui/ui.component";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, MediaComponent, StddevComponent],
+  imports: [RouterOutlet, UiComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
-export class AppComponent {
-  title = "unit-testing";
-}
+export class AppComponent {}
