be006d4cac92542ce79373154da3829bfc197d9e
harlock024 <hadith024@gmail.com>
2024-11-04 10:42:03 -0600

    simpson ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { MediaComponent } from "./media/media.component";
 import { StddevComponent } from "./stddev/stddev.component";
 import { LinearRegresionComponent } from "./linear-regression/linear-regression.component";
+import { SimpsonComponent } from "./simpson/simpson.component";
 
 export const routes: Routes = [
   { path: "media", component: MediaComponent },
   { path: "stddev", component: StddevComponent },
   { path: "linearRegresion", component: LinearRegresionComponent },
+  { path: "simpson", component: SimpsonComponent },
 ];
 
 @NgModule({
--- src/app/simpson/simpson.component.ts
+++ src/app/simpson/simpson.component.ts
@@ -1,12 +1,21 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { SimpsonRule } from "../common/simpson_rule";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-simpson",
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: "./simpson.component.html",
-  styleUrls: ["./simpson.component.css"],
 })
 export class SimpsonComponent {
+  x0: number = 0;
+  x1: number = 0;
+  numSeg: number = 0;
+  error: number = 0;
+  result: number | null = null;
+
   result1: number = 0;
   result2: number = 0;
   result3: number = 0;
@@ -19,5 +28,15 @@ export class SimpsonComponent {
     this.result1 = SimpsonRule.simpson(0, 4, 4, 0.0001, SimpsonRule.fx_2x);
     this.result2 = SimpsonRule.simpson(0, 1, 4, 0.0001, SimpsonRule.fx_x2);
     this.result3 = SimpsonRule.simpson(1, 4, 6, 0.001, SimpsonRule.fx_1_x);
+  }
+
+  simpson() {
+    this.result = SimpsonRule.simpson(
+      this.x0,
+      this.x1,
+      this.numSeg,
+      this.error,
+      SimpsonRule.fx_2x,
+    );
   }
 }
